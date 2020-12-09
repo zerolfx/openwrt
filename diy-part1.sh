a761709013d4a9f599167047ced64786cc29d5a2
zerolfx <zerolfx0@gmail.com>
2020-12-09 14:03:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# fullcone NAT patch
+git clone -b master --single-branch https://github.com/LGA1150/openwrt-fullconenat package/fullconenat
